b446430be2802f9548177579c3aa395dd0a64056
IIpocTo <allxf95@gmail.com>
2018-02-07 02:32:34 +0300

    Add Semantic UI

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import App from "./components/App/App";
+import "semantic-ui-css/semantic.min.css";
 
 ReactDOM.render(
   <App/>,
