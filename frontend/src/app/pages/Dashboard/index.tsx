95452ec1db0673eecf10cebc45c9c97af7d4ddb4
George <eakarpov@yandex.ru>
2018-03-03 00:15:55 +0300

    image to be checked

--- frontend/src/app/pages/Dashboard/index.tsx
+++ frontend/src/app/pages/Dashboard/index.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import Button from "semantic-ui-react/dist/commonjs/elements/Button/Button";
-import {Image} from "semantic-ui-react";
-import {Container} from "semantic-ui-react";
+import {Image, Card, Container} from "semantic-ui-react";
 import {RouteComponentProps} from "react-router";
 import {Component, ReactNode} from "react";
 import {Translate} from "react-redux-i18n";
@@ -24,6 +23,39 @@ class Dashboard extends Component<Props> {
               </Button>
             </ContainerWith25Margin>
           </FullWidthRail>
+        </NullMarginSegment>
+        <NullMarginSegment>
+          <Card.Group>
+            <Card>
+              <Card.Content>
+                <Card.Header>Matthew Harris</Card.Header>
+                <Card.Meta>Co-Worker</Card.Meta>
+                <Card.Description>Matthew is a pianist living in Nashville.</Card.Description>
+              </Card.Content>
+            </Card>
+
+            <Card>
+              <Card.Content>
+                <Card.Header content='Jake Smith' />
+                <Card.Meta content='Musicians' />
+                <Card.Description content='Jake is a drummer living in New York.' />
+              </Card.Content>
+            </Card>
+
+            <Card>
+              <Card.Content
+                header='Elliot Baker'
+                meta='Friend'
+                description='Elliot is a music producer living in Chicago.'
+              />
+            </Card>
+
+            <Card
+              header='Jenny Hess'
+              meta='Friend'
+              description='Jenny is a student studying Media Management at the New School'
+            />
+          </Card.Group>
         </NullMarginSegment>
         <Container>
           This is main page
