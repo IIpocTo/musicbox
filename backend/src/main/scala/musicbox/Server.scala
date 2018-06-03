dbf2bac55ff81750a4e1b0b03d4243e1a71012e3
IIpocTo <allxf95@gmail.com>
2018-06-03 06:14:14 +0300

    transfer server secret from code to config

--- backend/src/main/scala/musicbox/Server.scala
+++ backend/src/main/scala/musicbox/Server.scala
@@ -4,7 +4,6 @@ import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.stream.{ActorMaterializer, Materializer}
 import com.typesafe.config.{Config, ConfigFactory}
-import musicbox.session.SessionConfig
 
 import scala.concurrent.ExecutionContextExecutor
 import scala.io.StdIn
@@ -16,10 +15,6 @@ object Server extends App {
   implicit val materializer: Materializer = ActorMaterializer()
 
   def config: Config = ConfigFactory.load()
-
-  val sessionConfig = SessionConfig.default(
-    "jegor3lesrinf39t7mc5h6un6r0c69lgfno69dsak3vabeqamouq4328cuaekros401ajdpkh60rrtpd8ro24rbuqmgtnd1ebag6ljnb65i8a55d482ok7o0nckarpow"
-  )
 
   private val mainRouter = new HttpRoute()
   private val interface = config.getString("http.interface")
