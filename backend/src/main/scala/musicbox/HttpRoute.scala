23c27971c0599aaeedac1399047cb129ec94d246
IIpocTo <allxf95@gmail.com>
2018-06-04 01:26:29 +0300

    CORS config and some handlers

--- backend/src/main/scala/musicbox/HttpRoute.scala
+++ backend/src/main/scala/musicbox/HttpRoute.scala
@@ -1,7 +1,8 @@
 package musicbox
 
+import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Directives._
-import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.server.{Directive, ExceptionHandler, RejectionHandler, Route}
 import ch.megard.akka.http.cors.scaladsl.CorsDirectives._
 import musicbox.routes.AuthRouter
 import musicbox.session.directives.csrf.CsrfDirectives.randomTokenCsrfProtection
@@ -19,17 +20,33 @@ class HttpRoute(implicit executionContext: ExecutionContext) {
   implicit private val authService: AuthService = wire[AuthService]
   implicit private val authRouter: AuthRouter = wire[AuthRouter]
 
+  val rejectionHandler: RejectionHandler =
+    corsRejectionHandler.withFallback(RejectionHandler.default)
+
+  val exceptionHandler = ExceptionHandler {
+    case e: Exception => complete(StatusCodes.BadRequest -> e.getMessage)
+  }
+
+  val handleErrors: Directive[Unit] =
+  handleRejections(rejectionHandler) &
+  handleExceptions(exceptionHandler)
+
   val routes: Route =
-    cors() {
-      randomTokenCsrfProtection(checkHeader) {
-        pathPrefix("v1") {
-          authRouter.route ~
-            path("healthcheck") {
-              get {
-                complete("OK")
+    handleErrors {
+      cors() {
+        handleErrors {
+          randomTokenCsrfProtection(checkHeader) {
+            pathPrefix("v1") {
+              authRouter.route ~
+              path("healthcheck") {
+                get {
+                  complete("OK")
+                }
               }
             }
+          }
         }
       }
     }
+
 }
