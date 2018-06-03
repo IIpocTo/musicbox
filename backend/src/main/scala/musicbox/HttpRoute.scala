d5cab06b4bf4c250c9bd1b6d3954ac88acb63f1d
IIpocTo <allxf95@gmail.com>
2018-06-03 06:49:45 +0300

    fixes

--- backend/src/main/scala/musicbox/HttpRoute.scala
+++ backend/src/main/scala/musicbox/HttpRoute.scala
@@ -3,6 +3,9 @@ package musicbox
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 import musicbox.routes.AuthRouter
+import musicbox.session.directives.csrf.CsrfDirectives.randomTokenCsrfProtection
+import musicbox.session.directives.csrf.CsrfOptions.checkHeader
+import musicbox.MusicboxSessionManager._
 //import com.softwaremill.macwire._
 //import musicbox.db.UserDao
 //import musicbox.routes.AuthRouter
@@ -17,12 +20,14 @@ class HttpRoute(implicit executionContext: ExecutionContext) {
   private val authRouter = AuthRouter()
 
   val routes: Route =
-    pathPrefix("v1") {
-      authRouter.route
-    } ~
-      path("healthcheck") {
-        get {
-          complete("OK")
+    randomTokenCsrfProtection(checkHeader) {
+      pathPrefix("v1") {
+        authRouter.route ~
+        path("healthcheck") {
+          get {
+            complete("OK")
+          }
         }
       }
+    }
 }
--- backend/src/main/scala/musicbox/routes/AuthRouter.scala
+++ backend/src/main/scala/musicbox/routes/AuthRouter.scala
@@ -4,8 +4,6 @@ import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.{Directive0, Directive1, Route}
 import musicbox.session.SessionOptions._
-import musicbox.session.directives.csrf.CsrfDirectives._
-import musicbox.session.directives.csrf.CsrfOptions._
 import com.typesafe.scalalogging.StrictLogging
 import musicbox.SessionData
 import musicbox.MusicboxSessionManager._
@@ -25,29 +23,25 @@ case class AuthRouter()(implicit executionContext: ExecutionContext) extends Str
   val musicboxInvalidateSession: Directive0 = invalidateSession(refreshable, usingCookies)
 
   val route: Route =
-    randomTokenCsrfProtection(checkHeader) {
-      path("login") {
-        post {
-          entity(as[String]) { body =>
-            logger.info(s"Logging in $body")
-            musicboxSetSession(SessionData(body)) {
-              setNewCsrfToken(checkHeader) { ctx =>
-                ctx.complete(StatusCodes.OK, "ok")
-              }
-            }
-          }
-        }
-      } ~
-      path("logout") {
-        post {
-          musicboxRequiredSession { session =>
-            musicboxInvalidateSession { ctx =>
-              logger.info(s"Logging out $session")
-              ctx.complete(StatusCodes.OK, "ok")
-            }
-          }
+  path("login") {
+    post {
+      entity(as[String]) { body =>
+        logger.info(s"Logging in $body")
+        musicboxSetSession(SessionData(body)) { ctx =>
+          ctx.complete(StatusCodes.OK, "ok")
         }
       }
     }
+  } ~
+  path("logout") {
+    post {
+      musicboxRequiredSession { session =>
+        musicboxInvalidateSession { ctx =>
+          logger.info(s"Logging out $session")
+          ctx.complete(StatusCodes.OK, "ok")
+        }
+      }
+    }
+  }
 
 }
