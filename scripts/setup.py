39e27d09e82fe3e952654e660c67298ecac6ae1c
George <eakarpov@yandex.ru>
2018-06-08 04:10:20 +0300

    sketch for google music

--- scripts/setup.py
+++ scripts/setup.py
@@ -4,9 +4,10 @@ setup(name='spotify_importer',
       version='1.0',
       description='Spotify import data',
       author='George Carpow',
-      packages=['spotify_importer'],
+      packages=['spotify_importer', 'google_music'],
       install_requires=[
           'pymongo',
-          'spotipy'
+          'spotipy',
+          'gmusicapi'
       ],
       )
