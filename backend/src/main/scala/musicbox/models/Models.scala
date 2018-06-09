f87d0cef8afffd7e453a9b692d870c7f3c2202c9
daniilazarnov <azarnow.d@gmail.com>
2018-06-09 09:43:50 +0300

    naming

--- backend/src/main/scala/musicbox/models/Models.scala
+++ backend/src/main/scala/musicbox/models/Models.scala
@@ -71,7 +71,7 @@ object Models {
 
   case class AlbumResponseWithoutTracks(
    id: String,
-   artistsId: Seq[ArtistResponse],
+   artists: Seq[ArtistResponse],
    name: String,
    image: String,
    releaseDate: LocalDate,
